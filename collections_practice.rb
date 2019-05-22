cdd187c094004c2eb6ee00d6f162126f04d0ad76
Xinran Che <gracexinran@gmail.com>
2019-05-22 18:00:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,10 @@
 # your code goes here
 def begins_with_r(array)
   array.each do |element|
-    if element.start_with?('r')
+    if !element.downcase.start_with?('r')
+      return false
+    end
+  end
+  return true
+end
+      
