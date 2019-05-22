fec231f70c65e16cbb0eaca72525a8cb7d407e01
Xinran Che <gracexinran@gmail.com>
2019-05-22 18:01:37 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,4 +7,13 @@ def begins_with_r(array)
   end
   return true
 end
-      
+
+def contain_a(array)
+  result = []
+  array.each do |element|
+    if element.downcase.include?('a')
+      result << element
+    end
+  end
+  result
+end
